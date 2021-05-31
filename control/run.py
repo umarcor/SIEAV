f0d54a23c8864d94e16165ceb18d617ef08e5852
umarcor <unai.martinezcorral@ehu.eus>
2021-05-31 09:44:09 +0200

    control: support cosimulation with shared lib in any location

--- control/run.py
+++ control/run.py
@@ -16,10 +16,26 @@ LIB.add_source_files([
     ROOT / "test" / "*.vhd",
 ])
 
-check_call(["gcc", "-fPIC", "-shared", str(ROOT / "c" / "caux.c"), "-o", "libcaux.so"])
+check_call(["gcc", "-fPIC", "-shared", str(ROOT / "c" / "caux.c"), "-o",
+    # FIXME: it should be possible to use a custom location on Windows (MSYS2) or with LLVM backend, instead of PWD;
+    #        currently it works with mcode on Linux only
+    # str(ROOT / "libcaux.so")
+    "libcaux.so"
+])
+
+# FIXME: this should allow LLVM to compile on Linux, regardless of the custom location of libcaux.so;
+#        it does not; moreover, it cannot be used with mcode backend (-Wl is not supported)
+#for tb in LIB.get_test_benches(pattern="*tb_axi*", allow_empty=False):
+#    tb.set_sim_option("ghdl.elab_flags", ["-Wl,-L" + str(ROOT), "-Wl,-lcaux"])
+
+# FIXME: maybe these can be used on Windows (MSYS2)?
+#LDVAR = "PATH"
+#PSEP = ";"
 
 if name == 'posix':
-    LDPATH = environ.get("LD_LIBRARY_PATH")
-    environ["LD_LIBRARY_PATH"] = (LDPATH + ":" if LDPATH else '') + "."
+    LDVAR = "LD_LIBRARY_PATH"
+    LDPATH = environ.get(LDVAR)
+    # FIXME: libcaux.so should be located in ROOT, so here str(ROOT) should be added instead of "."
+    environ[LDVAR] = (LDPATH + ":" if LDPATH else '') + "."
 
 VU.main()
