1db4bcf35b2a573f1d93e909b09a1543cfb8463c
umarcor <unai.martinezcorral@ehu.eus>
2021-06-01 04:41:42 +0200

    add MWE

---
+++ issue-shared/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+set -e
+
+cd $(dirname "$0")
+
+ORIG_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+
+ghdl -a tb.vhd
+
+gcc -shared caux.c -o libcaux.so
+
+export LD_LIBRARY_PATH="$ORIG_LD_LIBRARY_PATH:$(pwd)"
+
+ghdl elab-run tb
+echo "Successfully run with libcaux.so"
+
+mkdir -p subdir
+
+mv libcaux.so subdir/
+
+export LD_LIBRARY_PATH="$ORIG_LD_LIBRARY_PATH:$(pwd)/subdir"
+
+ghdl elab-run tb
+echo "Successfully run with subdir/libcaux.so"
+
+rm -rf *.o *.cf *.so
---
+++ issue-shared/tb.vhd
@@ -0,0 +1,22 @@
+entity tb is
+end tb;
+
+architecture arch of tb is
+
+  function get_rand return integer is
+  begin report "VHPIDIRECT libcaux.so get_rand" severity failure; end;
+  attribute foreign of get_rand: function is "VHPIDIRECT libcaux.so get_rand";
+
+begin
+
+  process
+    variable v : integer;
+  begin
+    for i in 1 to 10 loop
+      v := get_rand;
+      report integer'image(i) & ": " & integer'image(v) severity note;
+    end loop;
+    wait;
+  end process;
+
+end;
