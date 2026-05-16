06e7072efe1dde109b32e832c60602ce95946e26
umarcor <unai.martinezcorral@ehu.eus>
2026-05-17 01:35:17 +0200

    logging: add synth script to synthesise rtl with GHDL

---
+++ logging/synth.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# Copyright 2026 Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
+#                University of the Basque Country (UPV/EHU) <ehu.eus>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+cd "$(dirname "$0")"
+
+mkdir -p build
+cd build
+
+# TODO
+# More recent versions of GHDL support `-o`, instead of having to redirect the output.
+
+ghdl --synth --std=08 --out=raw-vhdl -frelaxed \
+../rtl/rotation.vhd \
+-e rotation \
+> rotation.synth.vhd
